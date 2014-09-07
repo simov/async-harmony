63ffeba1bfa608dac1ca091c0f0575812e78ad84
simov <simeonvelichkov@gmail.com>
2014-09-07 12:24:48 +0300

    Fix parallelLimit

--- test/parallel.js
+++ test/parallel.js
@@ -79,6 +79,36 @@ describe('parallel', function () {
 describe('parallelLimit', function () {
     'use strict';
     
+    it('limit', (done) => {
+        var items = [1,2,3,4,5,6,7,8],
+            time  = [25,100,50,50,25,50,100,25],
+            index = 0;
+
+        async.parallelLimit(3, {
+            job1: (done) => {setTimeout(() => done(null, 'result1'), time[index++])},
+            job2: (done) => {setTimeout(() => done(null, 'result2'), time[index++])},
+            job3: (done) => {setTimeout(() => done(null, 'result3'), time[index++])},
+            job4: (done) => {setTimeout(() => done(null, 'result4'), time[index++])},
+            job5: (done) => {setTimeout(() => done(null, 'result5'), time[index++])},
+            job6: (done) => {setTimeout(() => done(null, 'result6'), time[index++])},
+            job7: (done) => {setTimeout(() => done(null, 'result7'), time[index++])},
+            job8: (done) => {setTimeout(() => done(null, 'result8'), time[index++])}
+        }, (err, result) => {
+            if (err) return done(err);
+            if (Object.keys(result)[2] == 'job5')
+                result.should.deep.equal({
+                    job1: 'result1', job3: 'result3', job5: 'result5', job4: 'result4',
+                    job2: 'result2', job8: 'result8', job6: 'result6', job7: 'result7'
+                });
+            else
+                // finish at the same time, actual order is not guaranteed
+                result.should.deep.equal({
+                    job1: 'result1', job3: 'result3', job4: 'result4', job5: 'result5',
+                    job2: 'result2', job8: 'result8', job6: 'result6', job7: 'result7'
+                });
+            done();
+        });
+    });
     it('return result when done', (done) => {
         async.parallelLimit(2, {
             job1: (done) => {
